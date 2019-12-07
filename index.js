fdbc7188eed833f4cb9e814515fbaa0213c626ca
sw3d <>
2019-12-06 21:36:52 -0800

    Removed animation.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let mouseX = 0, mouseY = 0;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
-init().then(animate);
+init().then(render);
 
 async function init() {
   let resp = await fetch(TM3D_URL);
@@ -22,7 +22,6 @@ async function init() {
   scene.fog = new THREE.Fog(0xffffff, 1, 10000);
 
   let material = new THREE.MeshNormalMaterial();
-
   group = new THREE.Group();
 
   for (let sb of tm3d) {
@@ -39,32 +38,19 @@ async function init() {
 
     mesh.matrixAutoUpdate = false;
     mesh.updateMatrix();
-
     group.add(mesh);
-
   }
 
   scene.add(group);
-
-  //
-
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-
-  //
-
   document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-  //
-
   window.addEventListener('resize', onWindowResize, false);
-
 }
 
 function onWindowResize() {
-
   windowHalfX = window.innerWidth / 2;
   windowHalfY = window.innerHeight / 2;
 
@@ -72,36 +58,24 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-
+  render();
 }
 
 function onDocumentMouseMove(event) {
-
   mouseX = (event.clientX - windowHalfX) * 10;
   mouseY = (event.clientY - windowHalfY) * 10;
-
-}
-
-//
-
-function animate() {
-
-  requestAnimationFrame(animate);
-
   render();
-
 }
 
 function render() {
-
-  let time = Date.now() * 0.001;
+  let time = 0;
 
   let rx = Math.sin(time * 0.7) * 0.5,
     ry = Math.sin(time * 0.3) * 0.5,
     rz = Math.sin(time * 0.2) * 0.5;
 
   camera.position.x += (mouseX - camera.position.x) * 0.05;
-  camera.position.y += (- mouseY - camera.position.y) * 0.05;
+  camera.position.y += (-mouseY - camera.position.y) * 0.05;
 
   camera.lookAt(scene.position);
 
@@ -110,5 +84,4 @@ function render() {
   group.rotation.z = rz;
 
   renderer.render(scene, camera);
-
 }
